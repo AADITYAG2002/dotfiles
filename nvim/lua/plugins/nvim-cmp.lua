fc88085be4cafcdbd57512a091bbc6f86b4051b3
AADITYAG2002 <aadityag2002@gmail.com>
2024-07-25 23:35:14 +0530

    add: nvim-cmp formatting

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,27 +1,55 @@
+local kind_icons = {
+    Text = "",
+    Method = "󰆧",
+    Function = "󰊕",
+    Constructor = "",
+    Field = "󰜢",
+    Variable = "󰀫",
+    Class = "󰠱",
+    Interface = "",
+    Module = "",
+    Property = "󰜢",
+    Unit = "",
+    Value = "󰎠",
+    Enum = "",
+    Keyword = "󰌋",
+    Snippet = "",
+    Color = "󰏘",
+    File = "󰈙",
+    Reference = "",
+    Folder = "󰉋",
+    EnumMember = "",
+    Constant = "󰏿",
+    Struct = "󰙅",
+    Event = "",
+    Operator = "󰆕",
+    TypeParameter = "󰅲",
+}
+
 return {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
     dependencies = {
         -- autocompletion
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-nvim-lua',
-        'hrsh7th/cmp-path',
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-nvim-lua",
+        "hrsh7th/cmp-path",
         --snippets
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-        'rafamadriz/friendly-snippets',
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
         {
-            'onsails/lspkind-nvim',
+            "onsails/lspkind-nvim",
             config = function()
-                require('lspkind').init()
+                require("lspkind").init()
             end,
-},
+        },
     },
     config = function()
-        local cmp = require('cmp')
-        local luasnip = require('luasnip')
-        local cmp_buffer = require('cmp_buffer')
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+        local cmp_buffer = require("cmp_buffer")
 
         require("luasnip.loaders.from_vscode").lazy_load()
         cmp.setup({
@@ -31,20 +59,27 @@ return {
                 end,
             },
             sources = cmp.config.sources({
-                {name = "nvim_lsp"},
-                {name = "luasnip"},
-                {name = "buffer"},
-                {name = "path"}
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
+                { name = "buffer" },
+                { name = "path" },
             }),
             window = {
                 completion = {
-                    border = 'rounded',
-                    winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+                    border = "rounded",
+                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
                 },
                 documentation = {
-                    border = 'rounded',
-                    winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+                    border = "rounded",
+                    winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
                 },
+            },
+            formatting = {
+                format = function(_, vim_item)
+                    -- Kind icons
+                    vim_item.kind = string.format("%s %s", kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+                    return vim_item
+                end,
             },
 
             mapping = cmp.mapping.preset.insert({
@@ -55,7 +90,7 @@ return {
                 ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
                 ["<C-e>"] = cmp.mapping.abort(), -- close completion window
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                ["<Tab>"] = cmp.mapping(function (fallback)
+                ["<Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
                     elseif luasnip.locally_jumpable(1) then
@@ -63,8 +98,8 @@ return {
                     else
                         fallback()
                     end
-                end, {"i", "s"}),
-                ["<S-Tab>"] = cmp.mapping(function (fallback)
+                end, { "i", "s" }),
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
                     elseif luasnip.locally_jumpable(-1) then
@@ -72,9 +107,8 @@ return {
                     else
                         fallback()
                     end
-                end, {"i", "s"})
-            })
+                end, { "i", "s" }),
+            }),
         })
     end,
 }
-
