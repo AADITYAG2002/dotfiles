e60431f0d48e5b01c41dbb70898197c1bb6db283
AADITYAG2002 <aadityag2002@gmail.com>
2024-07-29 23:48:44 +0530

    update: which-key

--- nvim/lua/plugins/dap/dap.lua
+++ nvim/lua/plugins/dap/dap.lua
@@ -3,12 +3,11 @@ return {
     dependencies = {
         "rcarriga/nvim-dap-ui",
         "nvim-neotest/nvim-nio",
-        "mfussenegger/nvim-dap-python"
+        "mfussenegger/nvim-dap-python",
     },
-    config = function ()
+    config = function()
         local dap = require("dap")
         local dapui = require("dapui")
-        
         require("dapui").setup()
 
         dap.listeners.before.attach.dapui_config = function()
@@ -26,7 +25,7 @@ return {
 
         -- setup debugger for c
         dap.adapters.cppdbg = {
-            id = 'cppdbg',
+            id = "cppdbg",
             type = "executable",
             command = "/opt/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
         }
@@ -36,100 +35,97 @@ return {
                 name = "Run executable (GDB)",
                 type = "cppdbg",
                 request = "launch",
-                program = function ()
+                program = function()
                     local path = vim.fn.input({
-                        prompt = 'Path to executable: ',
-                        default = vim.fn.getcwd() .. '/',
-                        completion = 'file'
+                        prompt = "Path to executable: ",
+                        default = vim.fn.getcwd() .. "/",
+                        completion = "file",
                     })
-                    return (path and path ~= '') and path or dap.ABORT
+                    return (path and path ~= "") and path or dap.ABORT
                 end,
                 cwd = "${workspaceFolder}",
                 stopOnEntry = false,
                 setupCommands = {
                     {
-                        text = '-enable-pretty-printing',
-                        description = 'enable pretty printing',
-                        ignoreFailures = false
+                        text = "-enable-pretty-printing",
+                        description = "enable pretty printing",
+                        ignoreFailures = false,
                     },
                 },
             },
             {
-                name = 'Attach to gdbserver :1233',
-                type = 'cppdbg',
-                request = 'launch',
-                MIMode = 'gdb',
-                midebuggerServerAddress = 'localhost:1233',
-                miDebuggerPath = '/usr/bin/gdb',
-                cwd = '${workspaceFolder}',
-                program = function ()
+                name = "Attach to gdbserver :1233",
+                type = "cppdbg",
+                request = "launch",
+                MIMode = "gdb",
+                midebuggerServerAddress = "localhost:1233",
+                miDebuggerPath = "/usr/bin/gdb",
+                cwd = "${workspaceFolder}",
+                program = function()
                     local path = vim.fn.input({
-                        prompt = 'Path to executable: ',
-                        default = vim.fn.getcwd() .. '/',
-                        completion = 'file'
+                        prompt = "Path to executable: ",
+                        default = vim.fn.getcwd() .. "/",
+                        completion = "file",
                     })
-                    return (path and path ~= '') and path or dap.ABORT
+                    return (path and path ~= "") and path or dap.ABORT
                 end,
-
             },
             {
-                name = 'Run executable with arguments (GDB)',
-                type = 'cppdbg',
-                request = 'launch',
+                name = "Run executable with arguments (GDB)",
+                type = "cppdbg",
+                request = "launch",
                 -- This requires special handling of 'run_last', see
                 -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
                 program = function()
                     local path = vim.fn.input({
-                        prompt = 'Path to executable: ',
-                        default = vim.fn.getcwd() .. '/',
-                        completion = 'file',
+                        prompt = "Path to executable: ",
+                        default = vim.fn.getcwd() .. "/",
+                        completion = "file",
                     })
 
-                    return (path and path ~= '') and path or dap.ABORT
+                    return (path and path ~= "") and path or dap.ABORT
                 end,
                 args = function()
                     local args_str = vim.fn.input({
-                        prompt = 'Arguments: ',
+                        prompt = "Arguments: ",
                     })
-                    return vim.split(args_str, ' +')
+                    return vim.split(args_str, " +")
                 end,
-                cwd = '${workspaceFolder}',
+                cwd = "${workspaceFolder}",
                 setupCommands = {
                     {
-                        text = '-enable-pretty-printing',
-                        description = 'enable pretty printing',
-                        ignoreFailures = false
+                        text = "-enable-pretty-printing",
+                        description = "enable pretty printing",
+                        ignoreFailures = false,
                     },
                 },
             },
             {
-                name = 'Attach to gdbserver :1233',
-                type = 'cppdbg',
-                request = 'launch',
-                MIMode = 'gdb',
-                midebuggerServerAddress = 'localhost:1233',
-                miDebuggerPath = '/usr/bin/gdb',
-                cwd = '${workspaceFolder}',
-                program = function ()
+                name = "Attach to gdbserver :1233",
+                type = "cppdbg",
+                request = "launch",
+                MIMode = "gdb",
+                midebuggerServerAddress = "localhost:1233",
+                miDebuggerPath = "/usr/bin/gdb",
+                cwd = "${workspaceFolder}",
+                program = function()
                     local path = vim.fn.input({
-                        prompt = 'Path to executable: ',
-                        default = vim.fn.getcwd() .. '/',
-                        completion = 'file'
+                        prompt = "Path to executable: ",
+                        default = vim.fn.getcwd() .. "/",
+                        completion = "file",
                     })
-                    return (path and path ~= '') and path or dap.ABORT
+                    return (path and path ~= "") and path or dap.ABORT
                 end,
-
             },
-
         }
         dap.configurations.c = dap.configurations.cpp
         dap.configurations.asm = dap.configurations.cpp
         ---setup for python---
-        require('dap-python').setup('~/.venv/debugpy/bin/python')
+        require("dap-python").setup("~/.venv/debugpy/bin/python")
 
-        vim.keymap.set('n', "<Leader>db", dap.toggle_breakpoint, {desc = "Toggle Breakpoint"})
-        vim.keymap.set('n', "<leader>dc", dap.continue, {desc = "continue debugging"})
-        vim.keymap.set('n', "<leader>du", dapui.toggle, {desc = "Dap UI"})
-        vim.keymap.set('n', "<leader>de", dapui.eval, {desc = "Eval"})
+        vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+        vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "continue debugging" })
+        vim.keymap.set("n", "<leader>du", dapui.toggle, { desc = "Dap UI" })
+        vim.keymap.set("n", "<leader>de", dapui.eval, { desc = "Eval" })
     end,
 }
